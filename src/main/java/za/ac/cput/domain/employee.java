8079ae8e735f96d879f8019b4cddcdc6f8bc1db3
Mxolisi Ndovela <yanela692@gmail.com>
2024-05-10 21:39:23 +0200

    first commit

---
+++ src/main/java/za/ac/cput/domain/employee.java
@@ -0,0 +1,67 @@
+package za.ac.cput.domain;
+
+/*
+ Full name:         Mxolisi Ndovela
+ Student number:    2017094740
+ Module:            Application Development Practice 3
+ */
+public class employee {
+    private  int employeeId;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
